5813822f95dfe757ca56a299d81e4f1c98225955
Mario Cardoso Henriques <maricard@c1r13s9.42porto.com>
2023-02-06 11:44:53 +0000

    mintalk

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,21 +6,44 @@
 /*   By: maricard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 08:43:07 by maricard          #+#    #+#             */
-/*   Updated: 2023/02/06 08:47:03 by maricard         ###   ########.fr       */
+/*   Updated: 2023/02/06 11:35:30 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
-#include "libft/libftprintf/ft_printf.h"
+#include "libft/ft_printf/ft_printf.h"
 #include <signal.h>
 #include <unistd.h>
+
+void	send_signal(int pid, char *str)
+{
+	int	i;
+	int	c;
+
+	i = 0;
+	while (*str)
+	{
+		c = *str;
+		while (i < 8)
+		{
+			if ((c & (1 << i)) == 0)
+				kill(pid, SIGUSR1);
+			else
+				kill(pid, SIGUSR2);
+			usleep(100);
+		i++;
+		}
+		str++;
+		i = 0;
+	}
+}
 
 int	main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
 		ft_printf("Client must receive 2 parameters: <PID> & <STRING>.\n");
-		ft_printf("Example: '12345' 'Hello :)'\n")
+		ft_printf("Example: '12345' 'Hello :)'\n");
 		ft_printf("Please try doing this task right.\n");
 	}
 	else
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,21 +6,43 @@
 /*   By: maricard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 08:41:15 by maricard          #+#    #+#             */
-/*   Updated: 2023/02/06 08:44:33 by maricard         ###   ########.fr       */
+/*   Updated: 2023/02/06 11:44:28 by maricard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
-#include "libft/libftprintf/ft_printf.h"
+#include "libft/ft_printf/ft_printf.h"
 #include <signal.h>
 #include <unistd.h>
 
+void	handler(int signal)
+{
+	static int	i;
+	static char	c;
+
+	if (signal == SIGUSR2)
+		c = c << 1;
+	else
+		c = c << 0;
+	i++;
+	if (i == 8)
+	{
+		ft_printf("%c", c);
+		i = 0;
+		c = 0;
+	}
+}
+
 int	main(void)
 {
+	struct	sigaction {
+		void	(*sa_handler)(int);
+	};
+	sigaction.sa_handler = handler;
 	ft_printf("SERVER PROCESS ID | %d\n", getpid());
 	while (1)
 	{
-		signal(SIGUSR1, handler);
-		signal(SIGUSR2, handler);
+		sigaction(SIGUSR1, &handler, NULL);
+		sigaction(SIGUSR2, &handler, NULL);
 	}
 }
